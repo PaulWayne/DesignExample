10a3e663dc8ae1cec84428f53d2770c7934fd847
PaulWayne <ocoulby@gmail.com>
2015-09-08 22:53:20 +0200

    My second commit

--- app/src/main/java/com/example/macbookpro/designexample/activity/activity/FragmentDrawer.java
+++ app/src/main/java/com/example/macbookpro/designexample/activity/activity/FragmentDrawer.java
@@ -1,6 +1,7 @@
 package com.example.macbookpro.designexample.activity.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -8,7 +9,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -99,12 +103,21 @@ public class FragmentDrawer extends Fragment {
         navigationDrawerAdapter = new NavigationDrawerAdapter(getData(),getActivity());
         recyclerView.setAdapter(navigationDrawerAdapter);
 
-       /* recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new ClickListener(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                    fragmentDrawerListener.onDrawerItemSelected(view,position);
+                    drawerLayout.closeDrawer(containerView);
+            }
 
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
         }));
-         */
-        return inflater.inflate(R.layout.fragment_drawer, container, false);
+
+        return layout;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -115,7 +128,7 @@ public class FragmentDrawer extends Fragment {
     }
     public static List<NavDrawerItem> getData(){
         List<NavDrawerItem> navDrawerItems = new ArrayList<>();
-// preparing navigation drawer items
+    // preparing navigation drawer items
         for(int i =0;i <titles.length;i++){
             NavDrawerItem navDrawerItem = new NavDrawerItem();
             navDrawerItem.setTitle(titles[i]);
@@ -134,6 +147,39 @@ public class FragmentDrawer extends Fragment {
                     + " must implement OnFragmentInteractionListener");
         }
     }
+    public void setUp(int fragmentId, DrawerLayout mdrawerLayout, final Toolbar toolbar) {
+        containerView = getActivity().findViewById(fragmentId);
+        drawerLayout = mdrawerLayout;
+        actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                toolbar.setAlpha(1 - slideOffset / 2);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        drawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                actionBarDrawerToggle.syncState();
+            }
+        });
+
+    }
+
 
     @Override
     public void onDetach() {
@@ -155,8 +201,55 @@ public class FragmentDrawer extends Fragment {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
     }
+    public static interface ClickListener{
+        public void onClick(View view,int position);
+        public void onLongClick(View view,int position);
+
+    }
     public interface FragmentDrawerListener {
         public void onDrawerItemSelected(View view, int position);
+    }
+    static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+         private GestureDetector gestureDetector;
+         private ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context,final  RecyclerView recyclerView, final ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+             View child = rv.findChildViewUnder(e.getX(),e.getY());
+             if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)){
+                 clickListener.onClick(child, rv.getChildAdapterPosition(child));
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
     }
 
 }
